0d709d64157399c958f4b3338228aea1eaf94ec7
codernotfound-219 <aswatt219@icloud.com>
2025-06-16 16:13:23 +0530

    fix: remove edge_case in locate_eligible_batch

--- src/greedy_dp/helper.rs
+++ src/greedy_dp/helper.rs
@@ -4,25 +4,16 @@ use crate::core::{Batch, BatchSchedule, Job};
 // This function locates the index of the first batch
 // that has a due_date that is greater than the current-job's
 // due_date.
-// It also handles an edge case where you need to start from the batch
-// that has the minimum min_due_time
 
 pub fn locate_eligible_batch(schedule: &BatchSchedule, due: u32) -> Option<usize> {
-    let min_index = schedule
-        .batches
-        .iter()
-        .enumerate()
-        .min_by_key(|(_, batch)| batch.min_due_time)
-        .map(|(index, _)| index)?;
-
     schedule
         .batches
         .iter()
         .enumerate()
-        .skip(min_index)
         .find(|(_, batch)| batch.min_due_time >= due)
         .map(|(index, _)| index)
 }
+
 // NOTE:
 // This function returns false if given job 
 // cannot be placed in the given batch
