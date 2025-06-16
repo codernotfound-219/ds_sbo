539eb095ae56eac33e8c02ea9610290e200ca966
codernotfound-219 <aswatt219@icloud.com>
2025-06-16 14:32:01 +0530

    feat: execution of insert_before - utils

--- src/greedy_dp/utils.rs
+++ src/greedy_dp/utils.rs
@@ -1,20 +1,55 @@
 use crate::core::{Job, BatchSchedule, Batch};
 use super::{cost_calculator::InsertAction, helper::size_check};
 
-// pub fn insert_before(schedule: &mut BatchSchedule, batch_index: usize, job: Job, actions: &Vec<InsertAction>) {
-//
-//     for action in actions.iter() {
-//         match action {
-//             &InsertAction::InsertInBatch { batch_index, job_code } => {
-//                 if job.code == job_code {
-//                     schedule.batches[batch_index].insert_end(job);
-//                 }
-//             },
-//             &InsertAction::PopAndCreateNewBatch { batch_index, job_code }
-//             &InsertAction::PopAndInsertInNextBatch { batch_index, job_code }
-//         }
-//     }
-// }
+pub fn insert_before(schedule: &mut BatchSchedule, job: Job, actions: &[InsertAction]) {
+    match actions[0] {
+        InsertAction::InsertInBatch { batch_index, job_code } => {
+            if job.code != job_code {
+                panic!("Job code didnt match for InsertAction::InsertInBatch, batch_index: {}, job_code: {}", batch_index, job_code);
+            }
+            
+            schedule.batches[batch_index].insert_end(job);
+        },
+        InsertAction::PopAndCreateNewBatch { batch_index: _, job_code: _ } => {
+            panic!("First action is not insertion of cur_job")
+        },
+        InsertAction::PopAndInsertInNextBatch { batch_index: _, job_code: _} => {
+            panic!("First action is not insertion of cur_job")
+        },
+    } 
+
+    for action in actions.iter().skip(1) {
+        match action {
+            InsertAction::InsertInBatch { batch_index: _, job_code: _} => {
+                panic!("Insert in Batch occurs twice in actions!");
+            },
+            InsertAction::PopAndCreateNewBatch { batch_index, job_code } => {
+                if let Some(popped_job) = schedule.batches[*batch_index].pop_job() {
+                    if popped_job.code != *job_code {
+                        panic!("Job code didnt match for InsertAction::PopAndCreateNewBatch, batch_index: {}, job_code: {}", batch_index, job_code);
+                    }
+
+                    let mut batch = Batch::new(batch_index+2);
+                    batch.insert_begin(popped_job);
+                    schedule.insert_at_position(batch_index+1, batch);
+                } else {
+                    panic!("Popped from an empty batch in fn: insert_before");
+                }
+            },
+            InsertAction::PopAndInsertInNextBatch { batch_index, job_code } => {
+                if let Some(popped_job) = schedule.batches[*batch_index].pop_job() {
+                    if popped_job.code != *job_code {
+                        panic!("Job code didnt match for InsertAction::PopAndInsertInNextBatch, batch_index: {}, job_code: {}", batch_index, job_code);
+                    }
+
+                    schedule.batches[batch_index+1].insert_begin(popped_job);
+                } else {
+                    panic!("Popped from an empty batch in fn: insert_before");
+                }
+            },
+        }
+    }
+}
 
 pub fn create_after(schedule: &mut BatchSchedule, batch_index: usize, job: Job) {
     let batch = &schedule.batches[batch_index];
