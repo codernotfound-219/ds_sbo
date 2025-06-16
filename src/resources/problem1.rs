ef8e53815b3b1f7952c796d2cee2e0bc05d41c34
codernotfound-219 <aswatt219@icloud.com>
2025-06-16 15:59:42 +0530

    feat: solution for problem1 added to resources

--- src/resources/problem1.rs
+++ src/resources/problem1.rs
@@ -1,4 +1,4 @@
-use crate::core::Job;
+use crate::core::{Batch, BatchSchedule, Job};
 
 pub fn job1() -> Job {
     Job::new(1, 1, 29, 9, 9)
@@ -29,4 +29,37 @@ pub fn job9() -> Job {
 }
 pub fn job10() -> Job {
     Job::new(10, 6, 38, 4, 7)
+}
+
+// Returns the solution of greed_dp on problem 1
+pub fn greed_dp_solution() -> BatchSchedule {
+    let mut batch1 = Batch::new(1);
+    let mut batch2 = Batch::new(2);
+    let mut batch3 = Batch::new(3);
+    let mut batch4 = Batch::new(4);
+    let mut batch5 = Batch::new(5);
+    let mut batch6 = Batch::new(6);
+    let mut batch7 = Batch::new(7);
+    let mut schedule = BatchSchedule::new();
+
+    batch1.insert(job5());
+    batch2.insert(job3());
+    batch3.insert(job8());
+    batch3.insert(job4());
+    batch4.insert(job7());
+    batch4.insert(job1());
+    batch5.insert(job9());
+    batch5.insert(job2());
+    batch6.insert(job10());
+    batch7.insert(job3());
+
+    schedule.insert_end(batch1);
+    schedule.insert_end(batch2);
+    schedule.insert_end(batch3);
+    schedule.insert_end(batch4);
+    schedule.insert_end(batch5);
+    schedule.insert_end(batch6);
+    schedule.insert_end(batch7);
+
+    schedule
 }
