31e274b28deef58a79ffb2d1e805581fd21fcc61
codernotfound-219 <aswatt219@icloud.com>
2025-06-23 16:27:13 +0530

    feat: deviation calculation

--- src/greedy_dp/deviation_calculator.rs
+++ src/greedy_dp/deviation_calculator.rs
@@ -1,25 +1,25 @@
 use crate::core::{BatchSchedule, Job};
 
-pub fn create_in(batch_index: usize, schedule: &BatchSchedule, job: &Job) -> i32 {
-    let prev_completion = if batch_index == 0 {
-        0
-    } else {
-        schedule.batches[batch_index - 1].completion_time
-    };
-
-    let mut release_date = job.release_date.max(prev_completion); // 14
-    let mut completion = release_date + job.processing_time; // 17
-    let mut deviation = job.due_date as i32 - completion as i32; // 5
-    let mut aggregate = 0;
+// NOTE:
+// this function calculates the deviation as defined the report, from the given batch index
+fn calculate_deviation(
+    batch_index: usize,
+    schedule: &BatchSchedule,
+    mut completion: u32,
+    mut deviation: i32,
+) -> i32 {
+    let mut aggregate = if deviation < 0 { deviation } else { 0 };
 
     for index in batch_index..schedule.batches.len() {
         let batch = &schedule.batches[index];
-        release_date = batch.release_date.max(completion);
+        let release_date = batch.release_date.max(completion);
         completion = release_date + batch.processing_time;
         let current_deviation = batch.min_due_time as i32 - completion as i32;
+
         if current_deviation < 0 {
-            aggregate += current_deviation;
+            aggregate += current_deviation
         }
+
         deviation = deviation.min(current_deviation);
     }
 
@@ -30,6 +30,35 @@ pub fn create_in(batch_index: usize, schedule: &BatchSchedule, job: &Job) -> i32
     }
 }
 
+fn insert_in(batch_index: usize, schedule: &BatchSchedule, job: &Job) -> i32 {
+    let batch = &schedule.batches[batch_index];
+    let release_date = job.release_date.max(batch.release_date);
+    let processing = job.processing_time.max(batch.processing_time);
+    let completion = release_date + processing;
+    let due = job.due_date.min(batch.min_due_time);
+    let deviation = due as i32 - completion as i32;
+
+    calculate_deviation(batch_index, schedule, completion, deviation)
+}
+
+// NOTE:
+// this function calculates the deviation due to create at the given index
+pub fn create_in(batch_index: usize, schedule: &BatchSchedule, job: &Job) -> i32 {
+    let prev_completion = if batch_index == 0 {
+        0
+    } else {
+        schedule.batches[batch_index - 1].completion_time
+    };
+
+    let release_date = job.release_date.max(prev_completion);
+    let completion = release_date + job.processing_time;
+    let deviation = job.due_date as i32 - completion as i32;
+
+    calculate_deviation(batch_index, schedule, completion, deviation)
+}
+
+// NOTE:
+// this function calculates the deviation due to creation at the end
 pub fn create_end(schedule: &BatchSchedule, job: &Job) -> i32 {
     let index = schedule.batches.len() - 1;
     let last_batch = &schedule.batches[index];
--- src/greedy_dp/solve.rs
+++ src/greedy_dp/solve.rs
@@ -1,4 +1,4 @@
-use super::{Decision, DecisionHistory};
+use super::{deviation_calculator, Decision, DecisionHistory};
 use crate::core::{Batch, BatchSchedule, Job};
 
 pub fn solve(list: &mut Vec<Job>) -> BatchSchedule {
@@ -19,7 +19,7 @@ pub fn solve(list: &mut Vec<Job>) -> BatchSchedule {
 pub fn recurse(k: usize, schedule: &BatchSchedule, job: &Job) -> Vec<DecisionHistory> {
     let mut decision_set: Vec<DecisionHistory> = Vec::new();
 
-    if k > schedule.batches.len() {
+    if k >= schedule.batches.len() {
         let release_date = job
             .release_date
             .max(schedule.batches[schedule.batches.len() - 1].completion_time);
@@ -33,5 +33,25 @@ pub fn recurse(k: usize, schedule: &BatchSchedule, job: &Job) -> Vec<DecisionHis
 
         return decision_set;
     }
+
+    for index in k..schedule.batches.len() {
+        let batch = &schedule.batches[index];
+        // TODO:  find deviation
+        // 1. insert in 
+        // 2. create in
+        
+        let deviation_create_in = deviation_calculator::create_in(index, schedule, job);
+    }
+
     decision_set
+}
+
+fn insert_in(batch_index: usize, schedule: &BatchSchedule, job: &Job) -> i32 {
+    let batch = &schedule.batches[batch_index];
+    let release_date = job.release_date.max(batch.release_date);
+    let processing = job.processing_time.max(batch.processing_time);
+    let completion = release_date + processing;
+    let due = job.due_date.min(batch.min_due_time);
+
+    let deviation = due as i32 - completion as i32;
 }
